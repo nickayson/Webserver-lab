8c4433ecf4dd257ac2dec66385410a66ff3476ca
Nicholas Ayson <aysonnicholas@gmail.com>
2021-10-08 11:18:32 -0700

    Update python code to get rid of Type error

--- Webserver/webserv.py
+++ Webserver/webserv.py
@@ -1,43 +1,46 @@
-# hello world is http://127.0.0.1:5501/helloworld.html 
+# hello world is http://127.0.0.1:5500/helloworld.html 
 
-#import socket module
+#import socket module for the library
 from socket import *
 import sys # In order to terminate the program
-serverHost = '127.0.0.1'
-serverPort = 5501
-serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-serverSocket.bind((serverHost, serverPort))
-serverSocket.listen(1)
-while True:
-#Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()#Fill in start #Fill in end
-    try:
-        message = connectionSocket.recv(1024)#Fill in start #Fill in end
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()#Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
-        connectionSocket.send("\r\n") 
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-    except IOError:
-        #Send response message for file not found
-        #Fill in start
-        #Fill in end
-        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
-        connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
-        #Close client socket
-        #Fill in start
-        #Fill in end
-        connectionSocket.close()
-        break
-serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+
+def webServer(port = 5500):
+    serverHost = '127.0.0.1' #server host or ip address for html is declared
+    serverPort = 5500 #port number for the server is saved
+
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    #Prepare a sever socket
+    serverSocket.bind((serverHost, serverPort)) #bind the host and port together
+    serverSocket.listen(1)
+
+    while True:
+        #Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()#accepts the connection
+        try:
+            message = connectionSocket.recv(1024)#receive the maximum amount of bits in message
+
+            filename = message.split()[1]
+            f = open(filename[1:])
+            outputdata = f.read() #read file line
+            #Send one HTTP header line into socket
+            connectionSocket.send("HTTP/1.1 200 OK\n\n".encode())    #gives the ok if connection was good
+
+            #Send the content of the requested file to the client
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
+
+        except IOError:
+            #Send response message for file not found
+            print("404 Not Found")
+            connectionSocket.send("HTTP/1.1 404 Not Found\n\n".encode()) #sends the 404 not found message if html connection doesnt match up
+            #Close client socket
+            connectionSocket.close()
+            break
+    serverSocket.close()
+    sys.exit()#Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+    webServer(5500)
